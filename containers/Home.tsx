3f4a2b914b02d9a9aae49c24c25c8943c84f34a0
Owen Lee <owen0004@e.ntu.edu.sg>
2023-09-02 01:49:12 +0800

    fix: generating new url not showing loading anim

--- containers/Home.tsx
+++ containers/Home.tsx
@@ -29,7 +29,6 @@ const Home = () => {
     }
 
     try {
-      setGeneratingShortedUrl(false);
       let data = await shtnrApiService.postShtnr(originalUrl);
       if (data) {
         setValidInput(true);
